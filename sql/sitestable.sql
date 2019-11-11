6d52a51a7dd0d414e717b2055e240b2af610f483
Augusto Martinez <acm_asd@hotmail.com>
2019-11-11 08:34:21 -0600

    Create sitestable.sql

---
+++ sql/sitestable.sql
@@ -0,0 +1,24 @@
+-- SQL statements
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET TIME_ZONE = "+00:00";
+
+-- Create table
+
+CREATE TABLE IT NOT EXISTS `siteStatus` (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  siteUrl VARCHAR(255),
+  siteStatus VARCHAR(10)
+) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci AUTO_INCREMENT = 1;
+
+
+-- Fill in example
+
+INSERT INTO `siteStatus` (
+  siteUrl,
+  siteStatus
+) 
+VALUES (
+  "https://www.mycoolsite.org",
+  "Up"
+);
