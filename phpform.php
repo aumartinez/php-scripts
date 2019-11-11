e10ffb6423e88f7a95a13cafb615ded8c2e9ed30
Augusto Martinez <acm_asd@hotmail.com>
2019-11-11 14:17:53 -0600

    Update phpform.php

--- phpform.php
+++ phpform.php
@@ -38,6 +38,10 @@ class Form {
 <!doctype html>
 <html>
   <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    
     <title>PHP forms</title>
     
     <style>
