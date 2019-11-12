24189411d11dfcc057ee1f86215f5b17d5cd99be
Augusto Martinez <acm_asd@hotmail.com>
2019-11-12 12:40:46 -0600

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,1 +1,36 @@
 //JS Script
+
+$(document).ready(function(){
+  //To Do
+  
+  $("#userForm").submit(function(evt){
+    let errors = validateForm();
+    
+    if (errors.length == 0) {
+      return true;
+    }
+    else {
+      evt.preventDefault();
+      return false;
+    }
+    
+  });
+  
+  function validateForm() {
+    let errorFields = [];
+    
+    if ($("#name").val() == "") {
+      errorFields.push("name");
+    }
+    
+    if ($("#email").val() == "") {
+      errorFields.push("email");
+    }
+    
+    if ($("#password").val() == "") {
+      errorFields.push("password");
+    }
+    
+    return errorFields;
+  }
+});
