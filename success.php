4b9d9914ac6c715bfed8ebfd7303b9c69ad44d9a
Augusto Martinez <acm_asd@hotmail.com>
2019-11-13 12:20:13 -0600

    Create success.php

---
+++ success.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    
+    <title>Thank you</title>
+    
+    <link rel="stylesheet" type="text/css" href="css/form.css" />
+    
+  </head>
+  <body>
+    <div class="wrapper">
+      <h1>
+        Thank you for registering
+      </h1>
+      
+      <hr />
+    </div>
+  </body>
+</html>
